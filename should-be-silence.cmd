0e4809a648716c90882a4f3ff8a888310b3cfc03
Dirk Arnez <smalldirkalex@gmail.com>
2023-11-10 22:57:33 +0800

    - upload files

--- should-be-silence.cmd
+++ should-be-silence.cmd
@@ -1,5 +1,15 @@
-ffmpeg -i input.mp3 -af "pan=stereo|c0=-c0|c1=-c1" input_invert.mp3
-ffmpeg -i input.mp3 -i input_invert.mp3  -filter_complex amix=inputs=2:duration=first:dropout_transition=3:normalize=0  &&^
-ffmpeg -i combined.mp3 -acodec pcm_s16le -f s16le combined.pcm
+@REM ffmpeg -i input.mp3 -af "pan=stereo|c0=-c0|c1=-c1" input_invert.mp3
+@REM ffmpeg -i input.mp3 -i input_invert.mp3  -filter_complex amix=inputs=2:duration=first:dropout_transition=3:normalize=0 combined.pcm &&^
+@REM ffmpeg -i combined.mp3 -acodec pcm_s16le -f s16le combined.pcm
 
+set PATH=%USERPROFILE%\Downloads\ffmpeg-6.0-full_build-shared\ffmpeg-6.0-full_build-shared\bin;
+ffmpeg -i 440.wav -af "aeval=-val(0)|-val(1)" 440_invert.wav
+ffmpeg -i 440.wav -i 440_invert.wav -filter_complex amix=inputs=2:duration=first:dropout_transition=3:normalize=0 combined.wav
+@REM ffmpeg -i combined.wav -acodec pcm_s16le -f s16le combined.pcm
+@REM ffmpeg -i combined.wav -af volumedetect -vn -sn -f null NUL 
+
+@REM ffmpeg -i combined.wav  -af ebur128=peak=true -f null -
+ffmpeg -i combined.wav -af "pan=1c|c0=c1,silencedetect=noise=-18dB:d=0.5" -f null -
+
+pause
 
